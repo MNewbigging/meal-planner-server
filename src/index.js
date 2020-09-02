44d15005b38e972362530a02fa54148293e882a6
Matt Newbigging <mnewbigging@opus2.com>
2020-09-02 18:34:52 +0100

    hello world test

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+// route handler for default home page
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+// start the express server
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
